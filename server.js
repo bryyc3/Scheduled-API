cdac4c1330523edf66e487b923fc5b3240877c94
Bryce King <bryceeking5@gmail.com>
2024-09-07 11:36:07 -0400

    Password is hashed and salted when aquired by server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import bcrypt from 'bcrypt';
 import  {getUsers, createUser, returningUser} from './database.js';
 
 
@@ -17,13 +18,16 @@ app.get("/users", async (req, res) => {
 });
 app.post('/create-user', async (req, res) => {
     const user = req.body;
+    user.password = await bcrypt.hash(user.password, 13);
     const alreadyUser = await returningUser(user.email);
     if (alreadyUser[0] == null){
         const userCreated = await createUser(user);
         res.json({'created' : true})
     }
     else{ res.json({'created' : false}) }
-});
+});//check if user email (from user data object received from form) already exists in database
+   //if email already exists return created as false
+   //if email does not yet exist store user information in database and return created as true
 
 app.listen(8000, () => {
     console.log('Server running on port 8080');
